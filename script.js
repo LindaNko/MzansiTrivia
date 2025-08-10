4603e2211da9a40b920427ca132e0caa4c59faae
Linda Nkosi <lindako63@outlook.com>
2025-08-10 06:27:26 +0200

    2nd commit images

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ const locations = [
   { 
     name: "Table Mountain", 
     hint: "Famous landmark in Cape Town", 
-    image: "assets/table-mountain.jpg" 
+    image: "images/Table-Mountain.jpg" 
   },
   { 
     name: "Kruger National Park", 
     hint: "Popular safari destination", 
-    image: "assets/kruger.jpg" 
+    image: "images/Kruger.jpg"  
   },
   { 
     name: "Durban", 
     hint: "Coastal city in KwaZulu-Natal", 
-    image: "assets/durban.jpg" 
+    image: "images/Durban.jpg" 
   },
   { 
     name: "Johannesburg", 
     hint: "Largest city in South Africa", 
-    image: "assets/johannesburg.jpg" 
+    image: "images/Johannesburg.jpg" 
   },
   { 
     name: "Robben Island", 
     hint: "Historic island near Cape Town", 
-    image: "assets/robben-island.jpg" 
+    image: "images/Robbe-Island.jpg" 
   },
   { 
     name: "Bloemfontein", 
     hint: "Judicial capital of South Africa", 
-    image: "assets/bloemfontein.jpg" 
+    image: "images/Bloemfonten.jpg"  
   }
 ];
 
